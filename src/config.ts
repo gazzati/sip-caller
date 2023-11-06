7e5c5005d4688800fe170799cc219a5935bdf73d
tgazzaev <tgazzaev@samokat.ru>
2023-11-06 19:15:46 +0300

    init commit

---
+++ src/config.ts
@@ -0,0 +1,32 @@
+import dotenv from "dotenv"
+import Joi from "joi"
+
+dotenv.config()
+
+const envVarsSchema = Joi.object({
+  ARI_HOST: Joi.string().default("localhost").description("Ari host"),
+  ARI_PORT: Joi.string().default("8088").description("Ari port"),
+  ARI_APP: Joi.string().default("app").description("Ari application name"),
+  ARI_USER: Joi.string().default("user").description("Ari user"),
+  ARI_PASSWORD: Joi.string().default("password").description("Ari password"),
+})
+  .unknown()
+  .required()
+
+const { error, value: envVars } = envVarsSchema.validate(process.env)
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`)
+}
+
+const config = {
+  ari: {
+    host: envVars.ARI_HOST,
+    port: envVars.ARI_PORT,
+    app: envVars.ARI_APP,
+    user: envVars.ARI_USER,
+    password: envVars.ARI_PASSWORD,
+  }
+}
+
+export default config
---
+++ src/consumer.ts
@@ -0,0 +1,69 @@
+import { client } from "websocket"
+
+import type { connection, Message } from "websocket"
+
+import config from  "./config"
+
+class Consumer {
+  private ws: client = new client()
+
+  private url = `ws://${config.ari.host}:${config.ari.port}/ari/events?api_key=${config.ari.user}:${config.ari.password}&app=${config.ari.app}`
+
+  public start(): void {
+    console.info("Started")
+
+    this.ws.connect(this.url)
+
+    this.ws.on("connectFailed", () => {
+      this.reconnect()
+    })
+
+    this.ws.on("connect", (connection: connection) => {
+      console.info('Successfully connected to ARI')
+
+      if (typeof process.send === "function") {
+        process.send("ready")
+        process.on("SIGINT", () => {
+          connection.close()
+          process.exit(0)
+        })
+      }
+
+      connection.on("error", error => {
+        console.error(error)
+        this.reconnect()
+      })
+
+      connection.on("close", code => {
+        console.error(`Connection close: ${code}`)
+        this.reconnect()
+      })
+
+      connection.on("message", (message: Message) => {
+        try {
+          if (message.type === "utf8") {
+            const json = JSON.parse(message.utf8Data)
+
+            // if (json.type === AriEventType.ApplicationReplaced) {
+            //   this.logger.info("Application replaced")
+            //   process.exit()
+            // }
+
+           console.log(message.utf8Data)
+          }
+        } catch (error) {
+          console.error(`Parsing error: ${(error as Error).message}`)
+        }
+      })
+    })
+  }
+
+  private reconnect(): void {
+    setTimeout(() => {
+      console.info(`Reconnect to ARI`)
+      this.ws.connect(this.url)
+    }, 5000)
+  }
+}
+
+export default Consumer
