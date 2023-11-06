588f9dc1548c5031c7da156b613f39beba304f01
tgazzaev <tgazzaev@samokat.ru>
2023-11-06 19:48:00 +0300

    add ARI

---
+++ src/ari/client.ts
@@ -0,0 +1,74 @@
+import axios from "axios"
+
+import config from "@root/config"
+import logger from "@root/logger"
+
+import type { AxiosInstance, AxiosError } from "axios"
+
+
+interface Error extends AxiosError<{ message?: string }> {}
+
+class Client {
+  private base = `http://${config.ari.host}:${config.ari.port}/ari`
+  private auth = { username: config.ari.user, password: config.ari.password }
+
+  readonly instance: AxiosInstance
+
+  constructor() {
+    this.instance = axios.create({
+      baseURL: this.base,
+      timeout: 10_000,
+      auth:  this.auth,
+      headers: {
+        Accept: "application/json",
+        // ...(options?.headers && options.headers)
+      }
+    })
+
+    this.errorHandler()
+  }
+
+  public async get(url: string): Promise<any> {
+    try {
+      const response = await this.instance.get(url)
+      return response.data
+    } catch (error) {
+      throw error as AxiosError
+    }
+  }
+
+  public async post(url: string, body?: any): Promise<any> {
+    try {
+      const response = await this.instance.post(url, body)
+      return response.data
+    } catch (error) {
+      throw error as AxiosError
+    }
+  }
+
+  public async delete(url: string): Promise<any> {
+    try {
+      const response = await this.instance.delete(url)
+      return response.data
+    } catch (error) {
+      throw error as AxiosError
+    }
+  }
+
+  private errorHandler() {
+    this.instance.interceptors.response.use(
+      response => response,
+      e => {
+        const error = e as Error
+
+        if (error?.response?.data?.message?.includes("Auth")) {
+          logger.error(error.response.data.message)
+        }
+
+        return Promise.reject(error)
+      }
+    )
+  }
+}
+
+export default Client
---
+++ src/ari/index.ts
@@ -0,0 +1,46 @@
+import Client from "@ari/client"
+
+import config from "@root/config"
+
+class Ari {
+  private app = config.ari.app
+  readonly api = new Client()
+
+  public async getChannels(): Promise<Array<any>> {
+    return await this.api.get("/channels")
+  }
+
+  public async createChannel(
+    endpoint = "",
+    channelId: string,
+    variables?: { [key: string]: string }
+  ): Promise<any> {
+    return await this.api.post(`/channels/create?endpoint=${endpoint}&app=${this.app}`, {
+      // formats: "ulaw,alaw",
+      channelId,
+      variables
+    })
+  }
+
+  public async removeChannel(channelId: string, reason?: string): Promise<any> {
+    try {
+      return await this.api.delete(`/channels/${channelId}?${reason ? `reason=${reason}` : ""}`)
+    } catch {}
+  }
+
+  public async createBridge(bridgeId: string, callUUID: string): Promise<any> {
+    return await this.api.post(`/bridges?${bridgeId ? `bridgeId=${bridgeId}&name=${callUUID}` : ""}`)
+  }
+
+  public async addChannelsToBridge(bridgeId: string, channels: Array<string>): Promise<void | Error> {
+    for (const channel of channels) {
+      await this.api.post(`/bridges/${bridgeId}/addChannel?channel=${channel}`)
+    }
+  }
+
+  public async dial(channelId: string, timeout = 1000) {
+    return await this.api.post(`/channels/${channelId}/dial?timeout=${timeout}`)
+  }
+}
+
+export default Ari
